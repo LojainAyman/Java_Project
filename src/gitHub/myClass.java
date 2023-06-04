78d1d45add1f5aa870cbc7c029550aed3317b93c
LojainAyman <lojainalmfleh@gmail.com>
2023-06-04 20:33:35 +0300

    update

---
+++ src/gitHub/myClass.java
@@ -0,0 +1,15 @@
+package gitHub;
+
+public class myClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String myName = "lojain";
+		
+		int myAge =25;
+		
+		
+	}
+
+}
